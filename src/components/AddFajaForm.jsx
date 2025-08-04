8a7b4475f925b62d61731be9bdec90bc3b801203
augustoalr <augustoalr@gmail.com>
2025-08-04 17:34:24 +0400

    Se agregó InputGroup al import de reat-bootstrap

--- src/components/AddFajaForm.jsx
+++ src/components/AddFajaForm.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, Button, Row, Col } from 'react-bootstrap';
+import { Form, Button, Row, Col, InputGroup } from 'react-bootstrap';
 
 const AddFajaForm = ({ newFaja, handleInputChange, handleSubmit, companies }) => {
   return (
