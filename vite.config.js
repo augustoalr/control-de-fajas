ffa176e6aa48c323cbc497abedbcaa5cb8f1e163
augustoalr <augustoalr@gmail.com>
2025-08-04 14:40:34 +0400

    refactor(build): Simplify vite config to isolate issue

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
-import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
-      manifest: {
-        name: 'Control de Fajas',
-        short_name: 'Fajas',
-        description: 'Aplicación para controlar el inventario de fajas.',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@supabase/supabase-js': path.resolve(__dirname, 'node_modules/@supabase/supabase-js/dist/index.mjs'),
-    },
-  },
+  plugins: [react()],
 });
